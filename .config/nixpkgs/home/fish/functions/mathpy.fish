51986ddda5dc714554062035059485b0c6487f24
mingmingrr <12855406+mingmingrr@users.noreply.github.com>
2021-10-26 15:33:36 -0400

    fish.mathpy: add constants

--- .config/nixpkgs/home/fish/functions/mathpy.fish
+++ .config/nixpkgs/home/fish/functions/mathpy.fish
@@ -3,6 +3,7 @@ prog = """
 import numpy as np
 from numpy import *
 import scipy as sp
+import scipy.constants as const
 import matplotlib.pyplot as plt
 import numpy.fft as fft
 import scipy.signal as signal
