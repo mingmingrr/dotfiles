9586cad78081a09b7b025f8be2505ac4e8c102a6
mingmingrr <12855406+mingmingrr@users.noreply.github.com>
2021-10-26 15:34:59 -0400

    fish._tmux_refresh_display: move to config

--- .config/nixpkgs/home/fish/config.fish
+++ .config/nixpkgs/home/fish/config.fish
@@ -4,3 +4,9 @@ set fish_features qmark-noglob stderr-nocaret
 set __done_min_cmd_duration 1000
 set __done_notification_urgency_level normal
 
+if set -q TMUX
+	function _tmux_refresh_display --on-event fish_preexec
+		set -x DISPLAY (tmux showenv DISPLAY | string replace DISPLAY= '')
+	end
+end
+
