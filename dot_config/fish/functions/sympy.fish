e70d70c3ef75d6c3c414cf8f4b6665b250b0f03b
mingmingrr <mingmingrr@gmail.com>
2025-01-25 03:23:17 -0500

    fish.sympy: import constants

--- dot_config/fish/functions/sympy.fish
+++ dot_config/fish/functions/sympy.fish
@@ -2,6 +2,8 @@ function sympy
 ipython --matplotlib=tk -i -c '\
 prog = """
 import sympy
+try: import scipy.constants as const
+except: pass
 sympy.init_session()
 """.strip("\n")
 print("Executing:", *(">>> " + i for i in prog.splitlines()), sep="\n")
